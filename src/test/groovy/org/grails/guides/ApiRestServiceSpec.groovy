2d9f4ac2e16ac15691c84ab0cb55085297044abd
Guillermo Campoy <guillermo.campoy@mercadolibre.com>
2018-09-25 17:47:41 -0300

    agrego POST

--- src/test/groovy/org/grails/guides/ApiRestServiceSpec.groovy
+++ src/test/groovy/org/grails/guides/ApiRestServiceSpec.groovy
@@ -1,8 +1,12 @@
 package org.grails.guides
 
 import com.mercadolibre.taperecorder.ApiRestService
+import com.mercadolibre.taperecorder.CassetteRecording
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClientBuilder
 import spock.lang.Specification
@@ -29,6 +33,29 @@ class ApiRestServiceSpec extends Specification {
                         "http://localhost:8888/buyingflow/api/users?client.id=457027154409645&caller.id=312424379"
                 )
         );
+
+        then: '200 got'
+        "HTTP/1.1 200 OK".equals( "HTTP/1.1 200 OK")
+    }
+
+    void testPayments_info() {
+        given: 'client'
+
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+
+        //acá o podemos hacer más prolijo y parametrizarlo
+        URI uri = new URIBuilder("http://localhost:8888/buyingflow/api/payment_options_info?client.id=457027154409645&caller.id=312424379&status=active")
+                .build()
+
+        String bodyToPost = "{\"items_to_buy\":[{\"item_id\":\"MLA677168828\",\"quantity\":1,\"unit_type\":null,\"variation_id\":null}]}"
+
+        HttpPost request = new HttpPost(uri)
+
+        request.addHeader("Content-Type", "application/json")
+        request.setEntity(new StringEntity(bodyToPost, "UTF-8"));
+
+        when: 'post launched'
+        CloseableHttpResponse response = client.execute(request)
 
         then: '200 got'
         "HTTP/1.1 200 OK".equals( "HTTP/1.1 200 OK")
